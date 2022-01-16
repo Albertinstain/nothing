d2daa55a32b7290a284ca0c5a42514466caf29c3
Albertinstain <97821974+Albertinstain@users.noreply.github.com>
2022-01-16 12:22:14 +0600

    hello

---
+++ index.php
@@ -0,0 +1,365 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Document</title>
+    <style>
+
+    </style>
+
+
+</head>
+
+<body>
+   
+<div class="full-website">
+
+<center >
+
+<div class="top">
+<table border="0">
+    <tr>
+        <td><img src="logo0.png" alt=""></td> <td style="font-size:25px;">bdsellprice.com</td>
+    </tr>
+</table>
+<center>
+
+<p class="middle"> All about washing Machine </p>
+</center>
+</div>
+
+    <div class="menu-table">
+    <table border="0">
+        <tr>
+            <td><a href="../index.php"></a> Home</td> <td> <a href="all-brand-fridge.php">All-brand</a> </td> <td> <a href="walton-fridge-price-in-bd.php">Walton</a> </td> <td> <a href="singer-fridge-price-in-bd.php">Singer</a> </td> <td> <a href="vision-fridge-price-in-bd.php">Vision</a> </td> <td><a href="hitachi-fridge-price-in-bd.php"> Hitachi</a> </td><td><a href="sharp-fridge-price-in-bd.php"> Sharp</a> </td>
+        </tr>
+    </table>
+
+    </div>
+
+    </center>
+           
+    <div class="body-area">
+    
+
+
+                              <table align="center" border=".02" cellpadding="1">
+                                  
+                                  <tr>
+
+
+                                      <td>
+                                          <img class="responsive"src="lg-fridge/lg-refrigerator-1.jpg" alt=""><br>
+                                       <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                    </td> 
+
+
+                                    <td>
+                                          <img class="responsive" src="lg-fridge/lg-refrigerator-2.jpg" alt=""><br>
+                                          <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                    </td>
+
+
+                                     <td>
+                                          <img  class="responsive" src="lg-fridge/lg-refrigerator-3.jpg" alt=""><br>
+                                          <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                    </td>
+
+
+
+                                  </tr>
+
+                                  <tr>
+
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-4.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-5.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-6.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+                                   
+                                 
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-7.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-8.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-9.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-10.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-11.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-12.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-13.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-14.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-15.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-16.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-17.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-18.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-19.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-20.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-21.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+                                 <tr>
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive"src="lg-fridge/lg-refrigerator-22.jpg" alt=""><br>
+                                  <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                 </td> 
+                                 
+                                 
+                                 <td>
+                                     <img class="responsive" src="lg-fridge/lg-refrigerator-23.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                 </td>
+                                 
+                                 
+                                 <td>
+                                     <img  class="responsive" src="lg-fridge/lg-refrigerator-23.jpg" alt=""><br>
+                                     <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                 <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                 </td>
+                                 
+                                 
+                                 
+                                 </tr>
+
+                                 
+                                 
+
+                                  
+
+                              </table> 
+
+
+
+
+ <table class="post" border="0" align="center">
+<tr>
+    <td style="width:770px;" id="j">
+    <h3>helllll l ll         llll</h3>
+    <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia consectetur dicta suscipit eos voluptatum deserunt cum praesentium iure. Quae doloribus alias fugit error ullam, commodi nihil reiciendis quo nisi totam!
+Commodi error facilis aliquam ea dignissimos et reprehenderit nulla necessitatibus distinctio accusantium nemo doloremque ab, quae iure non recusandae aliquid quam voluptates. Amet voluptates necessitatibus atque rerum neque, nam dolor.
+</p>
+</td>
+</tr>
+ </table>
+
+
+    </div>
+
+    
+    <div class="footer">
+<table align="center" border="1" cellpadding="1">
+                                  
+                                  <tr>
+
+
+                                      <td>
+                                       
+                                       <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>     
+                                    </td> 
+
+
+                                    <td>
+                                        
+                                          <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>                  
+                                    </td>
+
+
+                                     <td>
+                                        
+                                          <a href="">  <strong>Brand:Walton<br>Model:Wac-blele</strong><br> </a> 
+                                    <b>   Price:5000Tk <br>Size:4500Tk</b>  
+                                    </td>
+
+
+
+                                  </tr>
+
+                              </table> 
+
+
+                              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
+
